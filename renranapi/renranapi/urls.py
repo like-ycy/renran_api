039dabdd47609eee542d69ce85a29e76cc1dffaf
like-ycy <15210198546@163.com>
2022-01-07 11:14:02 +0800

    总路由中添加文章子应用的路由

--- renranapi/renranapi/urls.py
+++ renranapi/renranapi/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     re_path(r'uploads/(?P<path>.*)', serve, {"document_root": settings.MEDIA_ROOT}),
     path('users/', include('users.urls')),
     path('home/', include('home.urls')),
+    path('article/', include('article.urls')),
 ]
