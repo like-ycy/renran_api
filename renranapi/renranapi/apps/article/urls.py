34e1d0d641b345b4cdc7147626f404f04494e0c2
like-ycy <15210198546@163.com>
2022-01-11 21:00:17 +0800

    修改文章发布时间和文章内容

--- renranapi/renranapi/apps/article/urls.py
+++ renranapi/renranapi/apps/article/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     re_path("^collection/(?P<pk>\d+)/$", views.CollectionAPIView.as_view()),
     re_path("^collection/(?P<collection>\d+)/articles/$", views.ArticleAPIView.as_view()),
     re_path("^(?P<pk>\d+)/$", views.ArticleAPIView.as_view()),
+    re_path("^(?P<pk>\d+)/interval/$", views.ArticleInfoAPIView.as_view()),
+    re_path("^(?P<pk>\d+)/info/$", views.ArticleInfoAPIView.as_view()),
 ]
--- renranapi/renranapi/apps/article/views.py
+++ renranapi/renranapi/apps/article/views.py
@@ -1,7 +1,9 @@
+from django.utils import timezone as datetime
 from rest_framework import status
 from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import ArticleCollection, Article
 from .serializers import CollectionModelSerializer, ArticleModelSerializer
@@ -76,3 +78,47 @@ class ArticleAPIView(ListAPIView, CreateAPIView):
         article.collection_id = collection_id
         article.save()
         return Response({"detail": "移动文章成功!"}, status=status.HTTP_200_OK)
+
+
+class ArticleInfoAPIView(APIView):
+    """文章信息视图"""
+    permission_classes = [IsAuthenticated]
+
+    def patch(self, request, pk):
+        """修改文章发布时间"""
+        try:
+            article = Article.objects.get(pk=pk, is_deleted=False, is_show=True, user=self.request.user)
+        except Article.DoesNotExist:
+            return Response({"detail": "当前文章不存在！"}, status=status.HTTP_400_BAD_REQUEST)
+
+        # 判断文章是否已经发布
+        if article.is_public:
+            return Response({"detial": "已经发布的文章不能设置定时发布!"})
+
+        # 判断发布时间是否为未来时间
+        now_time = datetime.now().timestamp()
+        pub_time_str = request.data.get('pub_date')  # 字符串
+        # 将字符串转换为时间戳
+        put_time = datetime.datetime.strptime(pub_time_str, "%Y-%m-%d %H:%M:%S").timestamp()
+
+        if put_time <= now_time:
+            return Response({"detail": "定时发布的文章必须是未来的时间点!"}, status=status.HTTP_400_BAD_REQUEST)
+
+        article.pub_date = pub_time_str
+        article.save()
+
+        return Response({"detail": "定时发布文章设置成功!"})
+
+    def put(self, request, pk):
+        """文章内容修改保存"""
+        try:
+            article = Article.objects.get(pk=pk, is_deleted=False, is_show=True, user=self.request.user)
+        except Article.DoesNotExist:
+            return Response({"detail": "当前文章不存在！"}, status=status.HTTP_400_BAD_REQUEST)
+
+        article.title = request.data.get('title', '')
+        article.content = request.data.get('content', '')
+        article.html_content = request.data.get('html_content', '')
+        article.save()
+
+        return Response({"detail": "编辑文章保存成功!"})
