195a656758c02bc9e84d202dce95d939190e17f8
like-ycy <15210198546@163.com>
2022-01-14 16:26:10 +0800

    支付结果的通知

--- renranapi/renranapi/apps/payments/views.py
+++ renranapi/renranapi/apps/payments/views.py
@@ -1,7 +1,6 @@
 import random
 
 from alipay import AliPay
-from django.conf import settings
 from django.utils import timezone as datetime
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
@@ -9,6 +8,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from article.models import Article
+from renranapi.utils.tablestore1 import *
 from .models import Reward
 
 
@@ -67,3 +67,108 @@ class AlipayAPIView(APIView):
         url = settings.ALIPAY.get("gateway_url") + order_string
 
         return Response({"url": url, "out_trade_no": out_trade_no})
+
+
+class AlipayResultAPIView(APIView):
+    """支付结果的处理"""
+
+    def get1(self, request):
+        """同步回调结果的处理"""
+        data = request.query_params.dict()  # 把查询字符串转换成字典
+        signature = data.pop("sign")
+
+        alipay = self.get_alipay()
+
+        # 验证支付的同步通知结果
+        success = alipay.verify(data, signature)
+        if True:
+            # 完成支付结果的处理
+            # 根据订单号提取订单信息
+            out_trade_no = data.get("out_trade_no")
+            try:
+                reward = Reward.objects.get(out_trade_no=out_trade_no, status=False)
+            except Reward.DoesNotExist:
+                return Response({"detail": "当前打赏记录不存在或者已完成！"}, status=status.HTTP_400_BAD_REQUEST)
+
+            # 根据订单的付款状态进行修改
+            self.change_reward(data, reward)
+
+            # 返回结果
+            return Response({"detail": "支付处理成功！", "article_id": reward.article.id})
+
+        return Response({"detial": "支付结果参数有误！"}, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        """提供给客户端查询订单支付结果"""
+        out_trade_no = request.query_params.get("out_trade_no")
+        try:
+            reward = Reward.objects.get(out_trade_no=out_trade_no)
+        except Reward.DoesNotExist:
+            return Response({"detail": "当前打赏记录不存在！"}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({"status": reward.status})
+
+    def post(self, request):
+        """异步回调结果的处理"""
+        data = request.data
+        signature = data.pop("sign")
+
+        alipay = self.get_alipay()
+
+        # 验证支付的异步通知结果
+        success = alipay.verify(data, signature)
+        if success and data["trade_status"] in ("TRADE_SUCCESS", "TRADE_FINISHED"):
+            # 完成支付结果的处理
+            # 根据订单号提取订单信息
+            out_trade_no = data.get("out_trade_no")
+            try:
+                reward = Reward.objects.get(out_trade_no=out_trade_no)
+                if reward.status:
+                    return Response("success", content_type="text/plain")
+            except Reward.DoesNotExist:
+                return Response("success", content_type="text/plain")
+
+            self.change_reward(data, reward)
+
+            # 返回结果
+            return Response("success", content_type="text/plain")
+
+        return Response("fail", status=status.HTTP_400_BAD_REQUEST, content_type="text/plain")
+
+    def change_reward(self, data, reward):
+        # 根据订单的付款状态进行修改
+        reward.status = True
+        reward.trade_no = data.get("trade_no")
+        reward.save()
+
+        # 增加文章作者的资金
+        reward.article.user.money += reward.money
+        reward.article.user.save()
+
+        # 增加文章的赞赏人数
+        reward.article.reward_count += 1
+        reward.article.save()
+
+        # 记录用户的赞赏行为
+        primary_key = {"id": settings.LOG_TABLE_ID, "user_id": reward.user.id, "message_id": reward.article.id}
+        attribute_columns = {
+            "is_reward": 1,
+            "timestamp": datetime.now().timestamp(),
+        }
+        OTS().update_row("user_message_log_table", primary_key, attribute_columns)
+
+    def get_alipay(self):
+        # 读取秘钥文件的内容
+        app_private_key_string = open(settings.ALIPAY.get("app_private_key_path")).read()
+        alipay_public_key_string = open(settings.ALIPAY.get("alipay_public_key_path")).read()
+        # 根据支付宝sdk生成支付链接
+        alipay = AliPay(
+            appid=settings.ALIPAY.get("appid"),  # appid
+            app_notify_url=settings.ALIPAY.get("app_notify_url"),  # 默认回调url
+            app_private_key_string=app_private_key_string,
+            alipay_public_key_string=alipay_public_key_string,
+            sign_type=settings.ALIPAY.get("sign_type"),
+            debug=settings.ALIPAY.get("debug")
+        )
+
+        return alipay
